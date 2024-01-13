73da40bc72576a6fa974fe29f1785a57a22778fa
Harsh Agrawal <harsh.mobile231@gmail.com>
2024-01-13 17:14:39 +0530

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Tooltip from 'react-tooltip';
 import './global.css'
 import { Switch } from '@headlessui/react'
 import { RadioGroup } from '@headlessui/react'
